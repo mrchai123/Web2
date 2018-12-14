82bc37128c3e01db3484ae8db60830e3b220e6c6
mrchai123 <2438496032@qq.com>
2018-12-14 13:55:09 +0800

    new

---
+++ detail.js
@@ -0,0 +1,6 @@
+/*
+* @Author: dell
+* @Date:   2018-12-10 20:26:29
+* @Last Modified by:   dell
+* @Last Modified time: 2018-12-10 20:26:29
+*/
---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: dell
+* @Date:   2018-12-10 20:26:00
+* @Last Modified by:   dell
+* @Last Modified time: 2018-12-11 18:29:24
+*/
+var ul=document.getElementById('news-ul');
+console.log(ul);
